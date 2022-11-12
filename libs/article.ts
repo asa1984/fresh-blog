e3039522a9eca992b65abdda5020a0abfc90b42a
Asahi Sato <satoasa9913@gmail.com>
2022-11-12 16:40:38 +0900

    remove: comment

--- libs/article.ts
+++ libs/article.ts
@@ -3,27 +3,6 @@ import { join } from "$std/path/mod.ts";
 import { extract } from "$std/encoding/front_matter.ts";
 import { z } from "https://deno.land/x/zod@v3.19.1/index.ts";
 
-// export type Article = {
-//   slug: string;
-//   title: string;
-//   overview: string;
-//   publish: boolean;
-//   publishedAt: string;
-//   content: string;
-// };
-
-// // deno-lint-ignore no-explicit-any
-// const isValidArticle = (arg: any): arg is Article => {
-//   const { slug, title, overview, publish, publishedAt, content } =
-//     arg as Article;
-//   return typeof slug === "string" &&
-//     typeof title === "string" &&
-//     typeof overview === "string" &&
-//     typeof publish === "boolean" &&
-//     typeof publishedAt === "string" &&
-//     typeof content === "string";
-// };
-
 export async function getArticle(slug: string): Promise<Article | null> {
   const path = join("./posts/articles/", `${slug}.md`);
   try {
